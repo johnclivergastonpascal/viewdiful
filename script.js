e71252bd7aa2540a8d652e2f043a508f4bc25b0a
GP <johncliverpineda@gmail.com>
2025-12-04 10:44:00 -0400

    Deploy App Por Ahora

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let currentPage = 0;
 let hasMore = true; 
 
 /* URL de la API */
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://server-viewdiful.onrender.com/";
 
 /* Selectores cortos */
 const $ = id => document.getElementById(id);
